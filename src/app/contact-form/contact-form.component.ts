e7d3441a4b7e443569acb91a09149a2fef8f0a5f
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2019-08-03 14:26:28 +0530

    template Driven Form Completed

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -7,6 +7,15 @@ import { NgModel, NgForm } from '@angular/forms';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
+  selectedMailingOption = 2;
+  mailingOption = [
+    { id: 1, option: 'Email' },
+    { id: 2, option: 'SMS' },
+    { id: 3, option: 'WhatsApp' },
+    { id: 4, option: 'Post' },
+    { id: 5, option: 'Call' },
+    { id: 6, option: 'Hike' }
+  ];
 
   constructor() { }
 
@@ -18,6 +27,15 @@ export class ContactFormComponent implements OnInit {
   }
   formSubmit(form: NgForm) {
     console.log(form);
+    // form.controls.firstname.setErrors(
+    //   { serverError: true }
+    // );
+    // form.control.setErrors({
+    //   formCustomErr: {
+    //     status: true,
+    //     message: 'Some custom message'
+    //   }
+    // });
   }
 
 }
